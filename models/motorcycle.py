62a3dceb6bc745241c4323a35f604eeb37624d07
r1gor-dev <r1gor@bk.ru>
2026-09-23 01:43:53 +0300

    truck and motorcycle

---
+++ models/motorcycle.py
@@ -0,0 +1,10 @@
+from models.vehicle import Vehicle
+
+class Motorcycle(Vehicle):
+    def __init__(self,brand,model,year, has_sidecar):
+        super().__init__(brand,model,year)
+        self.has_sidecar=has_sidecar
+
+    def get_info(self):
+        sidecarout = "Да" if self.has_sidecar else "Нет"
+        return f"{super().get_info()} Коляска: {sidecarout}"
